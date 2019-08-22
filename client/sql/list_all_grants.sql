64456bb9a4c15740a7ed93f164b022039e966050
ikethecoder <ikethecoder@canzea.com>
2019-08-22 10:13:52 -0700

    Update list_all_grants.sql

--- client/sql/list_all_grants.sql
+++ client/sql/list_all_grants.sql
@@ -1,12 +1,13 @@
 
-SELECT * FROM information_schema.role_column_grants;
-SELECT * FROM information_schema.role_routine_grants;
-SELECT * FROM information_schema.role_table_grants;
-SELECT * FROM information_schema.role_udt_grants;
-SELECT * FROM information_schema.role_usage_grants;
+SELECT * FROM information_schema.role_column_grants WHERE grantee IN ('${USER}','PUBLIC');
+SELECT * FROM information_schema.role_routine_grants WHERE grantee IN ('${USER}','PUBLIC');
+SELECT * FROM information_schema.role_table_grants WHERE grantee IN ('${USER}','PUBLIC');
+SELECT * FROM information_schema.role_udt_grants WHERE grantee IN ('${USER}','PUBLIC');
+SELECT * FROM information_schema.role_usage_grants WHERE grantee IN ('${USER}','PUBLIC');
 -- SELECT grantee, specific_catalog, specific_schema, privilege_type, count(*) FROM information_schema.routine_privileges GROUP BY grantee, specific_catalog, specific_schema, privilege_type; 
 SELECT grantee, table_catalog, table_schema, count(*) 
        FROM information_schema.role_table_grants 
+       WHERE grantee IN ('${USER}','PUBLIC')
        GROUP BY grantee, table_catalog, table_schema;
 
 
